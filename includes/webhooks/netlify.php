722b303b631e044a73caa700a1699f6cec68d0a8
Wesley L. Handy <wesley.handy@cbn.org>
2020-06-19 14:24:04 -0400

    [1] update build hook to ignore changes to itself when triggering builds

--- includes/webhooks/netlify.php
+++ includes/webhooks/netlify.php
@@ -3,9 +3,11 @@
 if ( ! function_exists( 'call_build_hook_on_save' ) ) :
 
     function call_build_hook_on_save($post_id) {
-        if (wp_is_post_revision($post_id) || wp_is_post_autosave($post_id)) {
+        // ignore changes to the build hook (for now)
+        if ( get_field( 'netlify_hook_url', $post_id ) ) {
             return;
         }
+
         $netlify_hook_url = get_field('netlify_hook_url', 'netlify_settings') ?: '';
         if ( !empty( $netlify_hook_url ) ) :
             $response = Requests::post( $netlify_hook_url );
