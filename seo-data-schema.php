6c4f07eaeb1c84aae3bb84d6345a3b78fc088cc0
Wesley L. Handy <wesley.handy@cbn.org>
2020-07-06 20:40:32 -0400

    update version

--- seo-data-schema.php
+++ seo-data-schema.php
@@ -3,7 +3,7 @@
 Plugin Name: SEO-friendly Data-Schema with Options Pages and Netlify Build Trigger
 Plugin URI: https://www.wesleylhandy.net
 Description: Worpress Plugin to add SEO-friendly Books, Events, and FAQ data-schema with Options Pages using Advanced Custom Fields Pro, and optimized for connecting via graphql to Static Site Generators and triggering builds on Netlify.
-Version: 1.2.9
+Version: 1.2.10
 Author: Wesley L. Handy <wesley@wearecreativ.media>
 Author URI: https://www.wesleylhandy.net
 Text Domain: seo_data_schema
@@ -19,7 +19,7 @@ if ( ! class_exists('Seo_data_schema') ):
 
 class Seo_data_schema {
     /** @var string The plugin version number. */
-    var $version = '1.2.9';
+    var $version = '1.2.10';
     
     /**
 	 * __construct
